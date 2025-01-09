df93082ddebac117ace0267510ca401f18c05cb7
AlbertKhu <x_albert@mail.ru>
2025-01-09 23:25:02 +0400

    update

---
+++ src/main/java/ru/kata311/SpringBoot/dao/UserDao.java
@@ -0,0 +1,14 @@
+package ru.kata311.SpringBoot.dao;
+
+import ru.kata311.SpringBoot.model.User;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao {
+    List<User> getUsers();
+    void addUser(User user);
+    void deleteUser(Long id);
+    void editUser(User user);
+    Optional<User> findById(Long id);
+
+}
